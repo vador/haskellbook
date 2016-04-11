363aaa29fcbfac3fb71231e3da087ff2fb88c894
Damien Salvador <vador@grabeuh.com>
2016-04-11 17:21:47 +0200

    With prompt

--- hello-haskell/src/Main.hs
+++ hello-haskell/src/Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
+import System.IO
+
 import Hello
 
 main :: IO ()
 main =  do
+  hSetBuffering stdout NoBuffering
+  putStr "Please input your name: "
   name <- getLine
   sayHello name
 
