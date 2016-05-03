ad4dde38166a8155fdbada6ac5351f60b7f16ac2
Damien Salvador <vador@grabeuh.com>
2016-05-03 17:14:15 +0200

    First test ok

--- addition/Addition.hs
+++ addition/Addition.hs
@@ -2,5 +2,10 @@
 
 module Addition where
 
-sayHello :: IO ()
-sayHello = putStrLn "hello!"
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1 + 1) > 1 `shouldBe` True
