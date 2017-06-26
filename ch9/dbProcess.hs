4964fbbf456844d27c069b027eb65e699397496d
Damien Salvador <vador@grabeuh.com>
2017-06-26 17:35:49 +0200

    Create Database Functions

--- ch9/dbProcess.hs
+++ ch9/dbProcess.hs
@@ -18,5 +18,21 @@ theDatabase =
   ]
 
 filterDbDate :: [DatabaseItem] -> [UTCTime]
-filterDbDate = undefined
+filterDbDate [] = []
+filterDbDate (DbDate myDate:xs) = myDate : filterDbDate xs
+filterDbDate (_:xs) = filterDbDate xs
 
+filterDbNumber :: [DatabaseItem] -> [Integer]
+filterDbNumber [] = []
+filterDbNumber (DbNumber myNum:xs) = myNum : filterDbNumber xs
+filterDbNumber (_:xs) = filterDbNumber xs
+
+mostRecent :: [DatabaseItem] -> UTCTime
+mostRecent = maximum . filterDbDate
+
+sumDb :: [DatabaseItem] -> Integer
+sumDb  = (foldl (+) 0) . filterDbNumber
+
+avgDb :: [DatabaseItem] -> Double
+avgDb [] = 0
+avgDb x = (fromIntegral(sumDb(x)))/ (fromIntegral(length(filterDbNumber(x))))
